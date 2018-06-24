e1a4c6025d9875e1006a361be4ba765bcf0f686f
amos_doan <amos.doan@gmail.com>
2018-06-24 14:46:09 +0900

    Add anchor to mainPage & startPage

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -4,9 +4,9 @@
       <h1>Mossol 시작하기</h1>
       <p>Internet이 연결된 모든 곳에서 Mossol을 시작하세요! </p>
       <p>
-        <input type="button" class="btn btn-lg btn-default" id="desktop" value="Desktop PC" onclick="relocate_location(this.id)"/>
-        <input type="button" class="btn btn-lg btn-primary" id="smartphone" value="Android iOS" onclick="relocate_location(this.id)"/>
-        <input type="button" class="btn btn-lg btn-warning" id="smarttv" value="Smart TV" onclick="relocate_location(this.id)"/>
+        <input type="button" class="btn btn-lg btn-default" id="desktop" value="Desktop PC" @click="$router.push('/start#desktop_guide')"/>
+        <input type="button" class="btn btn-lg btn-primary" id="smartphone" value="Android iOS" @click="$router.push('/start#smartphone_guide')"/>
+        <input type="button" class="btn btn-lg btn-warning" id="smarttv" value="Smart TV" @click="$router.push('/start#smarttv_guide')"/>
       </p>
     </div>
     <h1>Mossol에 오신것을 환영합니다!</h1>
--- src/components/router.vue
+++ src/components/router.vue
@@ -12,6 +12,14 @@
   Vue.use(Router);
 
   export default new Router({
+    mode : 'history',
+    scrollBehavior: function(to, from, savedPosition) {
+        if (to.hash) {
+            return {selector: to.hash}
+        } else {
+            return {x: 0, y: 0}
+        }
+    },
     routes: [
       {path: '/', component: mainPage},
       {path: '/about', component: aboutPage},
